212586f95587669d27ae3da9aa670c75826cb033
Yajushi Srivastava <yajushi.sri@gmail.com>
2021-05-30 09:51:44 +0530

    Adds status.html and corresponding changes in css and js

--- js/main.js
+++ js/main.js
@@ -155,3 +155,60 @@ $(window).load(function(){
   });
 
 })
+
+// ========================================================================= //
+//  Card Js
+// ========================================================================= //
+
+var targetUrls = '{"targets": [' +
+'{"title":"Neuroscience News Science Magazine","image":"https://demos.codetipi.com/zeen/wp-content/uploads/sites/2/2020/06/da-box-mobile.jpg","url":"https://neurosciencenews.com/","filter":"read"},' + 
+'{"title":"Human Behavioral Biology - Robert Sapolsky","image":"https://i.ytimg.com/vi/NNnIGh9g6fA/hqdefault.jpg","url":"https://youtu.be/NNnIGh9g6fA","filter":"play"},' +
+'{"title":"Nancy MIT Course 9.11: The Human Brain","image":"https://i.ytimg.com/vi/i1pdQjdAndc/maxresdefault.jpg","url":"https://youtu.be/i1pdQjdAndc","filter":"play"},' + 
+'{"title":"Papers with Code - The latest in Machine Learning","image":"https://paperswithcode.com/static/index.jpeg","url":"https://paperswithcode.com/","filter":"research"},' + 
+'{"title":"LeetCode","image":"https://leetcode.com/static/images/LeetCode_Sharing.png","url":"https://leetcode.com/","filter":"code"},' + 
+'{"title":"PHIL304: Existentialism","image":"https://resources.saylor.org/og/PHIL304-1200x1200.png","url":"https://learn.saylor.org/course/PHIL304","filter":"read"},' +
+'{"title":"Machine Learning - Andrew Ng","image":"https://s3.amazonaws.com/coursera_assets/meta_images/generated/XDP/XDP~COURSE!~machine-learning/XDP~COURSE!~machine-learning.jpeg","url":"https://www.coursera.org/learn/machine-learning","filter":"learn"},' +
+'{"title":"Podcast - Huberman Lab","image":"https://is4-ssl.mzstatic.com/image/thumb/Podcasts114/v4/6d/46/e1/6d46e17a-a186-5ba4-c63a-129c570a2734/mza_7741805145491103965.jpg/1200x630wp.png","url":"https://www.youtube.com/channel/UC2D2CMWXMOVWx7giW1n3LIg","filter":"play"},' +
+'{"title":"Neuroscience Lectures - Dr. Chris Thompson","image":"https://yt3.ggpht.com/ytc/AAUvwng47hPFvA5epTcoR1OX7sme-2XSwhxP8MzGOcyn=s88-c-k-c0x00ffffff-no-rj","url":"https://www.youtube.com/channel/UCoDpMmSogY8KcDnc0MvFBBQ","filter":"play"},' +
+'{"title":"AI Engineering - Srivatsan Srinivasan","image":"https://i.ytimg.com/vi/ekHvVeNRFBI/maxresdefault.jpg","url":"https://www.youtube.com/channel/UCwBs8TLOogwyGd0GxHCp-Dw","filter":"learn"},' +
+'{"title":"Google Cloud Ready Facilitator Program","image":"https://google.qwiklabs.com/favicon-144.png","url":"https://google.qwiklabs.com/public_profiles/6bb5729e-5399-4215-948f-fa801d0711c0","filter":"code"},' +
+'{"title":"Papers in Neuroscience","image":"https://www.ncbi.nlm.nih.gov/corehtml/pmc/pmcgifs/pmc-logo-share.png","url":"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4640495/","filter":"research"},' +
+'{"title":"Data Structures & Algorithms, Level-up Course","image":"https://img-b.udemycdn.com/course/480x270/3642112_bd0f_10.jpg?secure=gpZ4b6jKhOgcswBG0Ra5jg%3D%3D%2C1622382460","url":"https://www.udemy.com/course/cpp-data-structures-algorithms-levelup-prateek-narang/","filter":"learn"},' + 
+'{"title":"Machine Learning A-Z (Python & R in Data Science Course)","image":"https://img-c.udemycdn.com/course/480x270/950390_270f_3.jpg?…oBvB~LRiocBywARNDPINvEvgg__&Key-Pair-Id=APKAITJV77WS5ZT7262A","url":"https://www.udemy.com/course/machinelearning/","filter":"learn"},' + 
+'{"title":"Deep Learning and Computer Vision A-Z: OpenCV, SSD & GANs","image":"https://img-b.udemycdn.com/course/480x270/1357844_25b2.jpg?secure=9V4_Wev18AoXCM4kv7mIJQ%3D%3D%2C1622382460","url":"https://www.udemy.com/course/computer-vision-a-z/","filter":"learn"}]}';
+
+// {"title":"","image":"","url":"","filter":""}
+
+
+var response = JSON.parse(targetUrls);
+
+for(let i = 0; i < response.targets.length; i++){
+
+  var cardImg = document.createElement('img');
+    cardImg.setAttribute('class', 'img card-img-top');
+    cardImg.setAttribute('src', response.targets[i].image)
+    
+    var cardTitle = document.createElement('h4');
+    cardTitle.textContent = response.targets[i].title;
+    cardTitle.setAttribute('class', 's-title card-title'); 
+    
+    var cardUrl = document.createElement('a');
+    cardUrl.setAttribute('class', 'card-ref');
+    cardUrl.setAttribute('href', response.targets[i].url);
+    cardUrl.appendChild(cardImg);
+    cardUrl.appendChild(cardTitle);
+    
+    var cardDiv = document.createElement('div');
+    cardDiv.setAttribute('class', 'card-body');
+    cardDiv.appendChild(cardUrl);
+    
+    var mainDiv = document.createElement('div');
+    mainDiv.setAttribute('class', 'shadow-lg rounded card card-block border-light portfolio-thumbnail col-lg-4 col-md-6 all ' + response.targets[i].filter);
+    mainDiv.setAttribute('style', 'width: 18rem; height: 16rem;');
+    mainDiv.appendChild(cardDiv);
+    mainDiv.id = "cardResearch" + i;
+    
+    var currentDiv = document.getElementById("cards"); 
+    currentDiv.appendChild(mainDiv);
+
+};
